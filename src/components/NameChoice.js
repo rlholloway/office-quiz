a9e6aefa49c63570451df3c26cbc1cfb7ac02240
Rachel <rh.rachelholloway@gmail.com>
2022-06-10 00:20:59 -0400

    started working on the array since the API won't work

--- src/components/NameChoice.js
+++ src/components/NameChoice.js
@@ -6,14 +6,11 @@ import { useState, useEffect, useRef } from "react";
 const NameChoice = (props) => {
     // Stateful variables
     // Once again tricky because first and last names are store separately
-    const [ randFirstOne, setRandFirstOne ] = useState("");
-    const [ randLastOne, setRandLastOne ] = useState(""); 
+    const [ randNameOne, setRandNameOne ] = useState("");
 
-    const [randFirstTwo, setRandFirstTwo] = useState("");
-    const [randLastTwo, setRandLastTwo] = useState(""); 
-
-    const [randFirstThree, setRandFirstThree] = useState("");
-    const [randLastThree, setRandLastThree] = useState(""); 
+    const [randNameTwo, setRandNameTwo] = useState("");
+ 
+    const [randNameThree, setRandNameThree] = useState("");
 
     // Function to shuffle array
     const shuffleArr = (array) => {
@@ -31,33 +28,158 @@ const NameChoice = (props) => {
 
     // Calling the API again SPECIFICALLY for the list of characters
     useEffect(() => {
-    axios({
-        method: "GET",
-        url: 'https://proxy.hackeryou.com',
-        responseType: 'json',
-        params: {
-            limit: 1,
-            reqUrl: "https://officeapi.dev/api/characters/",
-            proxyHeaders: {
-                "headers_params": 'value'
-            },
-            xmlToJSON: false
-        }
-    }).then((res) => {
-        // Storing character list into new array and choosing first three indexes (which now have random characters for our stateful variables)
-        const nameArray = res.data.data;
+        const quoteSource = [{
+            quote: "I'm not superstitious, but... I'm a little-stitious.",
+            name: "Michael Scott"
+        }, {
+                quote: "I. DECLARE. BANKRUPTCY!",
+                name: "Michael Scott"
+            }, {
+                quote: "If I had a gun with two bullets and I was in a room with Hitler, Bin Laden and Toby, I would shoot Toby twice.",
+                name: "Michael Scott"
+            }, {
+                quote: "Sometimes I'll start a sentence and I don't even know where it's going. I just hope I find it along the way.",
+                name: "Michael Scott"
+            }, {
+                quote: "I want people to be afraid of how much they love me.",
+                name: "Michael Scott"
+            }, {
+                quote: "There are a huge number of yeast infections in this county. Probably because we're downriver from that old bread factory.",
+                name: "Dwight Schrute"
+            }, {
+                quote: "I don't believe in coddling people. In the wild, there is no health care. In the wild, health care is, 'Ow, I hurt my leg! I can't run! A lion eats me and I'm dead!' Well, I'm not dead. I'm the lion. You're dead!",
+                name: "Dwight Schrute"
+            }, {
+                quote: "I love catching people in the act. That's why I always whip open doors.",
+                name: "Dwight Schrute"
+            }, {
+                quote: "In the end, the greatest snowball isn't a snowball at all. It's fear. Merry Christmas",
+                name: "Dwight Schrute"
+            }, {
+                quote: "How would I describe myself? Three words: hardworking, alpha male, jackhammer, merciless, insatiable.",
+                name: "Dwight Schrute"
+            }, {
+                quote: "One day, Michael came in complaining about a speed bump on the highway. I wonder who he ran over then.",
+                name: "Jim Halpert"
+            }, {
+                quote: "Last week, Dwight found half a joint in the parking lot. And as it turns out, Dwight finding drugs is more dangerous than most people using drugs.",
+                name: "Jim Halpert"
+            }, {
+                quote: "I mean, I always knew that the branch would shut down someday. I just figured it would be because Michael sold the building for some magic beans.",
+                name: "Jim Halpert"
+            }, {
+                quote: "I can't say whether Dunder Mifflin paper is less flammable, sir, but I can assure that it is certainly not more flammable.",
+                name: "Jim Halpert"
+            }, {
+                quote: "I gotta tell you, this baby is amazing. She gets me out of everything, and I… and I love her. I also love her very much.",
+                name: "Jim Halpert"
+            }, {
+                quote: "I suggested we flip a coin, but Angela said she doesn't like to gamble. Of course, by saying that she was gambling that I wouldn't smack her.",
+                name: "Pam Beesly"
+            }, {
+                quote: "Once every hour, someone is involved in an internet scam. That man is Michael Scott.",
+                name: "Pam Beesly"
+            }, {
+                quote: "I feel God in this Chili's tonight.",
+                name: "Pam Beesly"
+            }, {
+                quote: "It's performance review day, company-wide. Last year, my performance review started with Michael asking me what my hopes and dreams were, and it ended with him telling me he could bench-press 190 pounds. So, I don't really know what to expect.",
+                name: "Pam Beesly"
+            }, {
+                quote: "I was hoping for a righteous mob, and I ended up with Dwight and Nellie. But, they both have a mob mentality. And, I'm pretty sure Dwight has a pitchfork in his car.",
+                name: "Pam Beesly"
+            }, {
+                quote: "If you pray enough you can change yourself into a cat person.",
+                name: "Angela Martin"
+            }, {
+                quote: "I find the mystery genre disgusting. I hate being titillated.",
+                name: "Angela Martin"
+            }, {
+                quote: "You may ask me out to dinner. Nothing fancy or foreign, no bars, no patios, no vegetables, and no seafood.",
+                name: "Angela Martin"
+            }, {
+                quote: "I know that patience and loyalty are good, and virtuous traits. But sometimes I just think you need to grow a pair.",
+                name: "Angela Martin"
+            }, {
+                quote: "Every little boy fantasizes about his fairytale wedding.",
+                name: "Andy Bernard"
+            }, {
+                quote: "I'll be the number-two guy here in Scranton in six weeks. How? Name repetition, personality mirroring, and never breaking off a handshake. I'm always thinking one step ahead. Like a... carpenter... that makes stairs.",
+                name: "Andy Bernard"
+            }, {
+                quote: "Sorry I annoyed you with my friendship.",
+                name: "Andy Bernard"
+            }, {
+                quote: "You can't let a girl feel good about herself. It will backfire on you. Every compliment has to be backhanded. 'Oh I like your dress, but I'd like it more if you had prettier hair.'",
+                name: "Andy Bernard"
+            }, {
+                quote: "Could you for once just let us enjoy a party instead of making it about all your issues?",
+                name: "Kelly Kapoor"
+            }, {
+                quote: "I have a lot of questions. Number one, how dare you?",
+                name: "Kelly Kapoor"
+            }, {
+                quote: "I think sometimes people are really mean to the hot, popular girl.",
+                name: "Kelly Kapoor"
+            }, {
+                quote: "I don't talk trash, I talk smack. They're totally different. Trash talk is all hypothetical, like, 'Your mom is so fat she can eat the Internet.' But smack talk is happening like, right now, like, 'You're ugly and I know it for a fact 'cause I got the evidence right there.'",
+                name: "Kelly Kapoor"
+            }, {
+                quote: "Chad Flendermen. Just an easy going black guy, he knows the streets, yet he also went to Oxford. So... Just as comfortable on a motorcycle as he is on Air Force One. Oh and he's also the world's leading Egyptologist.",
+                name: "Toby Flenderson"
+            }, {
+                quote: "Actually, I didn't think it was appropriate to invite children since it's... You know, there's gambling and alcohol, and it's in our dangerous warehouse and it's a school night. And, you know, Hooters is catering. You know, is that enough? Should I keep going?",
+                name: "Toby Flenderson"
+            }, {
+                quote: "Every Halloween I tell him the same thing; you can't bring weapons into the office, and every year he says the same thing; as soon as I get my weapons back I'm gonna kill you.",
+                name: "Toby Flenderson"
+            }, {
+                quote: "It's like I used to tell my wife. I do not apologize unless I think I'm wrong, and if you don't like it, you can leave. And I say the same thing to my current wife, and I'll say it to my next one, too.”",
+                name: "Stanely Hudson"
+            }, {
+                quote: "Life is short. Drive fast and leave a sexy corpse. That's one of my mottos.",
+                name: "Stanley Hudson"
+            }, {
+                quote: "I have been trying to get on jury duty every year since I was 18 years old. To get to go sit it in an air conditioned room, downtown, judging people, while my lunch is paid for … that is the life.",
+                name: "Stanley Hudson"
+            }, {
+                quote: "I'm glad Michael's getting help. He has a lot of issues and he's stupid.",
+                name: "Phyllis Lapin"
+            }, {
+                quote: "You have a lot to learn about this town, sweetie.",
+                name: "Phyllis Lapin"
+            }, {
+                quote: "We have a gym at home. It's called the bedroom.",
+                name: "Phyllis Lapin"
+            }, {
+                quote: "I didn't realize how many of Angela's opinions I agreed with until she tried to have my kneecaps shattered for sleeping with her husband.",
+                name: "Oscar Martinez"
+            }, {
+                quote: "Angela's engaged to a gay man. As a gay man, I'm horrified. As a friend of Angela's, horrified. As a lover of elegant weddings, I'm a little excited.",
+                name: "Oscar Martinez"
+            }, {
+                quote: "The hospital will provide dictionaries. Bring a thesaurus.",
+                name: "Oscar Martinez"
+            }, {
+                quote: "Oh, I didn't realize we were doing trick questions. What's the safest way to go skiing? Don't ski.",
+                name: "Darryl Philbin"
+            }, {
+                quote: "I was there. That dude is not engaged. I'm not a big believer in therapy, but I'll go into my own pockets to cover his co-pay.",
+                name: "Darryl Philbin"
+            }, {
+                quote: "In the gang world, we use something called Fluffy Fingers. That's when somebody really gets in your face, you know, you just... start tickling them and then he starts tickling you.You know, pretty soon you're laughing and hugging. Before you know it, you've forgotten the whole thing. Ya'll can just go to church together... get an ice cream cone.",
+                name: "Darryl Philbin"
+        }];
+
+        const nameArray = quoteSource;
         shuffleArr(nameArray);
-        
-        setRandFirstOne(nameArray[0].firstname);
-        setRandLastOne(nameArray[0].lastname);
 
-        setRandFirstTwo(nameArray[1].firstname);
-        setRandLastTwo(nameArray[1].lastname);
+        setRandNameOne(nameArray[0].name);
 
-        setRandFirstThree(nameArray[2].firstname);
-        setRandLastThree(nameArray[2].lastname);
-    });
-    }, []);
+        setRandNameTwo(nameArray[1].name);
+
+        setRandNameThree(nameArray[2].name);
+    },[]);
 
     // Grabs the DIV that holds the buttons and shuffles the content for them
     const ref = useRef(null);
@@ -75,10 +197,10 @@ const NameChoice = (props) => {
     return (
         // DIV which holds four buttons for multiple choice answer
         <div ref={ref} className="parent wrapper">
-            <button className="wrong" onClick={props.handleWrong}>{randFirstOne} {randLastOne}</button>
-            <button className="wrong" onClick={props.handleWrong}>{randFirstTwo} {randLastTwo}</button>
-            <button className="wrong" onClick={props.handleWrong}>{randFirstThree} {randLastThree}</button>
-            <button className="answer" onClick={props.handleClick}>{props.speakerFirst} {props.speakerLast}</button>
+            <button className="wrong" onClick={props.handleWrong}>{randNameOne}</button>
+            <button className="wrong" onClick={props.handleWrong}>{randNameTwo}</button>
+            <button className="wrong" onClick={props.handleWrong}>{randNameThree}</button>
+            <button className="answer" onClick={props.handleClick}>{props.name}</button>
         </div>
     )
 }
--- src/pages/OfficeQuote.js
+++ src/pages/OfficeQuote.js
@@ -10,8 +10,8 @@ import QuestionCounter from "../components/QuestionCounter";
 const OfficeQuote = () => {
     // Stateful variables
     const [quote, setQuote] = useState([]);
-    const [speakerFirst, setSpeakerFirst] = useState([]);
-    const [speakerLast, setSpeakerLast] = useState([]);
+    const [name, setName] = useState([]);
+    
     const [answerClicked, setAnswerClicked] = useState(false);
     const [wrongClicked, setWrongClicked] = useState(false);
     const [counter, setCounter] = useState(0);
@@ -19,28 +19,156 @@ const OfficeQuote = () => {
 
     // useEffect to stop infinite loop of re-rendering
     useEffect(() => {
-        axios({
-            method: "GET",
-            url: 'https://proxy.hackeryou.com',
-            responseType: 'json',
-            params: {
-                limit: 1,
-                reqUrl: "https://officeapi.dev/api/quotes/random",
-                proxyHeaders: {
-                    "headers_params": 'value'
-                },
-                xmlToJSON: false
-            }
-        }).then((res) => {
-            // Store data in variables
-            // Character data is tricky because first and last names are separated in API
-            setQuote(res.data.data.content);
-            setSpeakerFirst(res.data.data.character.firstname);
-            setSpeakerLast(res.data.data.character.lastname);
-        });
-        // API will re-render after user clicks either right or wrong answer
-    }, [answerClicked, wrongClicked]);
+        const quoteSource = [{
+            quote: "I'm not superstitious, but... I'm a little-stitious.",
+            name: "Michael Scott"
+        }, {
+            quote: "I. DECLARE. BANKRUPTCY!",
+            name: "Michael Scott"
+        }, {
+            quote: "If I had a gun with two bullets and I was in a room with Hitler, Bin Laden and Toby, I would shoot Toby twice.",
+            name: "Michael Scott"
+        }, {
+            quote: "Sometimes I'll start a sentence and I don't even know where it's going. I just hope I find it along the way.",
+            name: "Michael Scott"
+        }, {
+            quote: "I want people to be afraid of how much they love me.",
+            name: "Michael Scott"
+        }, {
+            quote: "There are a huge number of yeast infections in this county. Probably because we're downriver from that old bread factory.",
+            name: "Dwight Schrute"
+        }, {
+            quote: "I don't believe in coddling people. In the wild, there is no health care. In the wild, health care is, 'Ow, I hurt my leg! I can't run! A lion eats me and I'm dead!' Well, I'm not dead. I'm the lion. You're dead!",
+            name: "Dwight Schrute"
+        }, {
+            quote: "I love catching people in the act. That's why I always whip open doors.",
+            name: "Dwight Schrute"
+        }, {
+            quote: "In the end, the greatest snowball isn't a snowball at all. It's fear. Merry Christmas",
+            name: "Dwight Schrute"
+        }, {
+            quote: "How would I describe myself? Three words: hardworking, alpha male, jackhammer, merciless, insatiable.",
+            name: "Dwight Schrute"
+        }, {
+            quote: "One day, Michael came in complaining about a speed bump on the highway. I wonder who he ran over then.",
+            name: "Jim Halpert"
+        }, {
+            quote: "Last week, Dwight found half a joint in the parking lot. And as it turns out, Dwight finding drugs is more dangerous than most people using drugs.",
+            name: "Jim Halpert"
+        }, {
+            quote: "I mean, I always knew that the branch would shut down someday. I just figured it would be because Michael sold the building for some magic beans.",
+            name: "Jim Halpert"
+        }, {
+            quote: "I can't say whether Dunder Mifflin paper is less flammable, sir, but I can assure that it is certainly not more flammable.",
+            name: "Jim Halpert"
+        }, {
+            quote: "I gotta tell you, this baby is amazing. She gets me out of everything, and I… and I love her. I also love her very much.",
+            name: "Jim Halpert"
+        }, {
+            quote: "I suggested we flip a coin, but Angela said she doesn't like to gamble. Of course, by saying that she was gambling that I wouldn't smack her.",
+            name: "Pam Beesly"
+        }, {
+            quote: "Once every hour, someone is involved in an internet scam. That man is Michael Scott.",
+            name: "Pam Beesly"
+        }, {
+            quote: "I feel God in this Chili's tonight.",
+            name: "Pam Beesly"
+        }, {
+            quote: "It's performance review day, company-wide. Last year, my performance review started with Michael asking me what my hopes and dreams were, and it ended with him telling me he could bench-press 190 pounds. So, I don't really know what to expect.",
+            name: "Pam Beesly"
+        }, {
+            quote: "I was hoping for a righteous mob, and I ended up with Dwight and Nellie. But, they both have a mob mentality. And, I'm pretty sure Dwight has a pitchfork in his car.",
+            name: "Pam Beesly"
+        }, {
+            quote: "If you pray enough you can change yourself into a cat person.",
+            name: "Angela Martin"
+        }, {
+            quote: "I find the mystery genre disgusting. I hate being titillated.",
+            name: "Angela Martin"
+        }, {
+            quote: "You may ask me out to dinner. Nothing fancy or foreign, no bars, no patios, no vegetables, and no seafood.",
+            name: "Angela Martin"
+        }, {
+            quote: "I know that patience and loyalty are good, and virtuous traits. But sometimes I just think you need to grow a pair.",
+            name: "Angela Martin"
+        }, {
+            quote: "Every little boy fantasizes about his fairytale wedding.",
+            name: "Andy Bernard"
+        }, {
+            quote: "I'll be the number-two guy here in Scranton in six weeks. How? Name repetition, personality mirroring, and never breaking off a handshake. I'm always thinking one step ahead. Like a... carpenter... that makes stairs.",
+            name: "Andy Bernard"
+        }, {
+            quote: "Sorry I annoyed you with my friendship.",
+            name: "Andy Bernard"
+        }, {
+            quote: "You can't let a girl feel good about herself. It will backfire on you. Every compliment has to be backhanded. 'Oh I like your dress, but I'd like it more if you had prettier hair.'",
+            name: "Andy Bernard"
+        }, {
+            quote: "Could you for once just let us enjoy a party instead of making it about all your issues?",
+            name: "Kelly Kapoor"
+        }, {
+            quote: "I have a lot of questions. Number one, how dare you?",
+            name: "Kelly Kapoor"
+        }, {
+            quote: "I think sometimes people are really mean to the hot, popular girl.",
+            name: "Kelly Kapoor"
+        }, {
+            quote: "I don't talk trash, I talk smack. They're totally different. Trash talk is all hypothetical, like, 'Your mom is so fat she can eat the Internet.' But smack talk is happening like, right now, like, 'You're ugly and I know it for a fact 'cause I got the evidence right there.'",
+            name: "Kelly Kapoor"
+        }, {
+            quote: "Chad Flendermen. Just an easy going black guy, he knows the streets, yet he also went to Oxford. So... Just as comfortable on a motorcycle as he is on Air Force One. Oh and he's also the world's leading Egyptologist.",
+            name: "Toby Flenderson"
+        }, {
+            quote: "Actually, I didn't think it was appropriate to invite children since it's... You know, there's gambling and alcohol, and it's in our dangerous warehouse and it's a school night. And, you know, Hooters is catering. You know, is that enough? Should I keep going?",
+            name: "Toby Flenderson"
+        }, {
+            quote: "Every Halloween I tell him the same thing; you can't bring weapons into the office, and every year he says the same thing; as soon as I get my weapons back I'm gonna kill you.",
+            name: "Toby Flenderson"
+        }, {
+            quote: "It's like I used to tell my wife. I do not apologize unless I think I'm wrong, and if you don't like it, you can leave. And I say the same thing to my current wife, and I'll say it to my next one, too.”",
+            name: "Stanely Hudson"
+        }, {
+            quote: "Life is short. Drive fast and leave a sexy corpse. That's one of my mottos.",
+            name: "Stanley Hudson"
+        }, {
+            quote: "I have been trying to get on jury duty every year since I was 18 years old. To get to go sit it in an air conditioned room, downtown, judging people, while my lunch is paid for … that is the life.",
+            name: "Stanley Hudson"
+        }, {
+            quote: "I'm glad Michael's getting help. He has a lot of issues and he's stupid.",
+            name: "Phyllis Lapin"
+        }, {
+            quote: "You have a lot to learn about this town, sweetie.",
+            name: "Phyllis Lapin"
+        }, {
+            quote: "We have a gym at home. It's called the bedroom.",
+            name: "Phyllis Lapin"
+        }, {
+            quote: "I didn't realize how many of Angela's opinions I agreed with until she tried to have my kneecaps shattered for sleeping with her husband.",
+            name: "Oscar Martinez"
+        }, {
+            quote: "Angela's engaged to a gay man. As a gay man, I'm horrified. As a friend of Angela's, horrified. As a lover of elegant weddings, I'm a little excited.",
+            name: "Oscar Martinez"
+        }, {
+            quote: "The hospital will provide dictionaries. Bring a thesaurus.",
+            name: "Oscar Martinez"
+        }, {
+            quote: "Oh, I didn't realize we were doing trick questions. What's the safest way to go skiing? Don't ski.",
+            name: "Darryl Philbin"
+        }, {
+            quote: "I was there. That dude is not engaged. I'm not a big believer in therapy, but I'll go into my own pockets to cover his co-pay.",
+            name: "Darryl Philbin"
+        }, {
+            quote: "In the gang world, we use something called Fluffy Fingers. That's when somebody really gets in your face, you know, you just... start tickling them and then he starts tickling you.You know, pretty soon you're laughing and hugging. Before you know it, you've forgotten the whole thing. Ya'll can just go to church together... get an ice cream cone.",
+            name: "Darryl Philbin"
+        }];
 
+        const sourceLength = quoteSource.length;
+        const randomNumber = Math.floor(Math.random() * sourceLength);
+
+        // Set a new quote
+        setQuote(quoteSource[randomNumber].quote)
+        setName(quoteSource[randomNumber].name)
+    }, [answerClicked, wrongClicked]);
 
     // Click event to call API again AND add points
     const handleClick = () => {
@@ -80,8 +208,7 @@ const OfficeQuote = () => {
                     <NameChoice
                         handleClick={handleClick}
                         handleWrong={handleWrong}
-                        speakerFirst={speakerFirst}
-                        speakerLast={speakerLast} />
+                        name={name} />
                 </div>
             </div>
 
